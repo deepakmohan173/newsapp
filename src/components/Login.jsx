524f9542dd1955b252ff1a50f0158cf69b270a43
Deepak <deepakmohan173@gmail.com>
2022-07-21 20:31:50 +0530

    testing axios

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -3,45 +3,45 @@ import React from "react";
 const Login = () => {
   return (
     <div>
-      <section class="h-full gradient-form bg-gray-200 md:h-screen">
-        <div class="container py-12 px-6 h-full">
-          <div class="flex justify-center items-center flex-wrap h-full g-6 text-gray-800">
-            <div class="xl:w-10/12">
-              <div class="block bg-white shadow-lg rounded-lg">
-                <div class="lg:flex lg:flex-wrap g-0">
-                  <div class="lg:w-6/12 px-4 md:px-0">
-                    <div class="md:p-12 md:mx-6">
-                      <div class="text-center">
+      <section className="h-full flex justify-center gradient-form bg-white md:h-screen">
+        <div className="container py-12 px-6 h-full">
+          <div className="flex justify-center items-center flex-wrap h-full g-6 text-gray-800">
+            <div className="xl:w-10/12">
+              <div className="block bg-white shadow-lg rounded-lg">
+                <div className="lg:flex lg:flex-wrap g-0">
+                  <div className="lg:w-6/12 px-4 md:px-0">
+                    <div className="md:p-12 md:mx-6">
+                      <div className="text-center">
                         <img
-                          class="mx-auto w-48"
+                          className="mx-auto w-48"
                           src="https://www.gstatic.com/images/branding/product/1x/keep_48dp.png"
                           alt="logo"
                         />
-                        <h4 class="text-xl font-semibold mt-1 mb-12 pb-1">
+                        <h4 className="text-xl font-semibold mt-1 mb-12 pb-1 font-euclid_medium ">
                           The News App
                         </h4>
                       </div>
                       <form>
-                        <p class="mb-4">Please login to your account</p>
-                        <div class="mb-4">
+                        <p className="mb-4">Please login to your account</p>
+                        <div className="mb-4">
                           <input
                             type="text"
-                            class="form-control block w-full px-3 py-1.5 text-base font-normal text-gray-700 bg-white bg-clip-padding border border-solid border-gray-300 rounded transition ease-in-out m-0 focus:text-gray-700 focus:bg-white focus:border-blue-600 focus:outline-none"
+                            className="form-control block w-full px-3 py-1.5 text-base font-normal text-gray-700 bg-white bg-clip-padding border border-solid border-gray-300 rounded transition ease-in-out m-0 focus:text-gray-700 focus:bg-white focus:border-blue-600 focus:outline-none"
                             id="exampleFormControlInput1"
                             placeholder="Username"
                           />
                         </div>
-                        <div class="mb-4">
+                        <div className="mb-4">
                           <input
                             type="password"
-                            class="form-control block w-full px-3 py-1.5 text-base font-normal text-gray-700 bg-white bg-clip-padding border border-solid border-gray-300 rounded transition ease-in-out m-0 focus:text-gray-700 focus:bg-white focus:border-blue-600 focus:outline-none"
+                            className="form-control block w-full px-3 py-1.5 text-base font-normal text-gray-700 bg-white bg-clip-padding border border-solid border-gray-300 rounded transition ease-in-out m-0 focus:text-gray-700 focus:bg-white focus:border-blue-600 focus:outline-none"
                             id="exampleFormControlInput1"
                             placeholder="Password"
                           />
                         </div>
-                        <div class="text-center pt-1 mb-12 pb-1">
+                        <div className="text-center pt-1 mb-12 pb-1">
                           <button
-                            class="inline-block px-6 py-2.5 bg-sky-800 text-black font-medium text-xs leading-tight uppercase rounded shadow-md hover:bg-blue-700 hover:shadow-lg focus:shadow-lg focus:outline-none focus:ring-0 active:shadow-lg transition duration-150 ease-in-out w-full mb-3"
+                            className="inline-block px-6 py-2.5 bg-sky-800 text-black font-medium text-xs leading-tight uppercase rounded shadow-md hover:bg-blue-700 hover:shadow-lg focus:shadow-lg focus:outline-none focus:ring-0 active:shadow-lg transition duration-150 ease-in-out w-full mb-3"
                             type="button"
                             data-mdb-ripple="true"
                             data-mdb-ripple-color="light"
