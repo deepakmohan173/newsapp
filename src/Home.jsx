5dda856f380ec7d6fe5c90458230becf56fbd281
Arun S V <svarunid@gmail.com>
2022-07-24 18:24:43 +0530

    Search component separated

--- src/Home.jsx
+++ src/Home.jsx
@@ -1,6 +1,7 @@
 import React, { useEffect, useRef, useState } from "react";
 import Blog from "./components/Blog";
 import axios from "axios";
+import {SearchBar} from "./components/SearchBar"
 
 const Home = () => {
   const [blogs, setBlogs] = useState([]);
@@ -24,65 +25,9 @@ const Home = () => {
       });
   }, []);
 
-  function SearchBar() {
-    const inputRef = useRef(null);
-    const handleSubmit = (e) => {
-      e.preventDefault();
-      const query = inputRef.current.value;
-      setLoading(true);
-      axios
-        .get("http://localhost:8080/news?query=" + query)
-        .then((res) => {
-          setSearchResult(res.data);
-          setLoading(false);
-        })
-        .catch((err) => {
-          console.log(err);
-        });
-    };
-    return (
-      <div className="m-24">
-        <form onSubmit={handleSubmit}>
-          <div className="relative">
-            <div className="flex absolute inset-y-0 left-0 items-center pl-3 pointer-events-none">
-              <svg
-                aria-hidden="true"
-                className="w-5 h-5 text-gray-500"
-                fill="none"
-                stroke="currentColor"
-                viewBox="0 0 24 24"
-                xmlns="http://www.w3.org/2000/svg"
-              >
-                <path
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                  strokeWidth="2"
-                  d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z"
-                ></path>
-              </svg>
-            </div>
-            <input
-              ref={inputRef}
-              type="search"
-              id="default-search"
-              className="block p-4 pl-10 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 focus:outline-blue-400"
-              placeholder="Search News"
-              required
-            ></input>
-            <button
-              type="submit"
-              className="text-white absolute right-2.5 bottom-2.5 bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-4 py-2 "
-            >
-              Search
-            </button>
-          </div>
-        </form>
-      </div>
-    );
-  }
   return (
     <div>
-      <SearchBar />
+      <SearchBar setLoading={setLoading} setSearchResult={setSearchResult}/>
       <div
         className={
           loading === true
